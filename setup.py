6b1c234c2f54c5af9e111295e6039b28e423dd4e
Sathvikdevineni <73529936+Sathvikdevineni@users.noreply.github.com>
2021-07-31 18:10:18 +0100

    added setup.py

---
+++ setup.py
@@ -0,0 +1,13 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Sat Jul 31 17:59:14 2021
+
+@author: Sai Sathvik
+"""
+
+from setuptools import setup
+
+setup(name='gym_coptor',
+      version='0.0.1',
+      install_requires=['gym']  # And any other dependencies foo needs
+)
